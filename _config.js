724775ad0506490ce5b1b40e7b0c56221353bf2b
dgichaba <dorothygichaba@gmail.com>
2023-03-10 23:50:35 +0300

    update db URL

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://dgichaba:<password>@mycluster1.jdcsfzz.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://dgichaba:<password>@mycluster1.jdcsfzz.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://dgichaba:<password>@mycluster1.jdcsfzz.mongodb.net/?retryWrites=true&w=majority',
+    production: 'mongodb+srv://dgichaba:D0r0thyCher0tich@mycluster1.jdcsfzz.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://dgichaba:D0r0thyCher0tich@mycluster1.jdcsfzz.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://dgichaba:D0r0thyCher0tich@mycluster1.jdcsfzz.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
